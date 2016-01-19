864516a1556b69e6c199da07ae63e965032da512
ianis@ianislallemand.net <ianis@ianislallemand.net>
2016-01-19 19:49:27 +0100

    send osc

--- app.py
+++ app.py
@@ -12,16 +12,14 @@ def app():
     dxlIO = pypot.dynamixel.DxlIO(ports[0])
     availableIDs = dxlIO.scan()
     server = OSCServer(('0.0.0.0', 8000))
-    server.timeout = 0
-    server.handleTimeout = types.MethodType(handleTimeout, server) # Funny python's way to add a method to an instance of a class
     for motorID in availableIDs:
         server.addMsgHandler('/motor/' + str(motorID), motorHandler) # Register OSC handlers for each found ID
     client = OSCClient()
     client.connect(('localhost', 8001))
     print 'Ready. Found ' + str(availableIDs) + ' ID(s)'
     while True:
-        sleep(1) # Here we simulate a game engine. First we do the game stuff...
-        processFrame() # ... then call user script
+        server.handle_request()
+        sleep(0.01)
 
 def motorHandler(addr, tags, data, source):
     # Ranges are given for AX-18 motors. For other models, see:
@@ -31,58 +29,55 @@ def motorHandler(addr, tags, data, source):
     if (data[0] == 'factory_reset'):
        dxlIO.factory_reset()
     elif (data[0] == 'get_control_mode'):
-        m = OSCMessage('/motor/' + str(motorID))
-        m.append(dxlIO.get_control_mode([motorID])[0])
-        client.send(m)
-        print dxlIO.get_control_mode([motorID])[0]
+        sendResponse(motorID, dxlIO.get_control_mode([motorID])[0])
     elif (data[0] == 'get_alarm_led'):
-        print dxlIO.get_alarm_LED([motorID])[0]
+        sendResponse(motorID, dxlIO.get_alarm_LED([motorID])[0])
     elif (data[0] == 'get_alarm_shutdown'):
-        print dxlIO.get_alarm_shutdown([motorID])[0]
+        sendResponse(motorID, dxlIO.get_alarm_shutdown([motorID])[0])
     elif (data[0] == 'get_angle_limit'):
-        print dxlIO.get_angle_limit([motorID])[0]
+        sendResponse(motorID, dxlIO.get_angle_limit([motorID])[0])
     elif (data[0] == 'get_compliance_margin'):
-        print dxlIO.get_compliance_margin([motorID])[0]
+        sendResponse(motorID, dxlIO.get_compliance_margin([motorID])[0])
     elif (data[0] == 'get_compliance_slope'):
-        print dxlIO.get_compliance_slope([motorID])[0]
+        sendResponse(motorID, dxlIO.get_compliance_slope([motorID])[0])
     elif (data[0] == 'get_drive_mode'):
-        print dxlIO.get_drive_mode([motorID])[0]
+        sendResponse(motorID, dxlIO.get_drive_mode([motorID])[0])
     elif (data[0] == 'get_firmware'):
-        print dxlIO.get_firmware([motorID])[0]
+        sendResponse(motorID, dxlIO.get_firmware([motorID])[0])
     elif (data[0] == 'get_goal_position'):
-        print dxlIO.get_goal_position([motorID])[0]
+        sendResponse(motorID, dxlIO.get_goal_position([motorID])[0])
     elif (data[0] == 'get_goal_position_speed_load'):
-        print dxlIO.get_goal_position_speed_load([motorID])[0]
+        sendResponse(motorID, dxlIO.get_goal_position_speed_load([motorID])[0])
     elif (data[0] == 'get_highest_temperature_limit'):
-        print dxlIO.get_highest_temperature_limit([motorID])[0]
+        sendResponse(motorID, dxlIO.get_highest_temperature_limit([motorID])[0])
     elif (data[0] == 'get_max_torque'):
-        print dxlIO.get_max_torque([motorID])[0]
+        sendResponse(motorID, dxlIO.get_max_torque([motorID])[0])
     elif (data[0] == 'get_moving_speed'):
-        print dxlIO.get_moving_speed([motorID])[0]
+        sendResponse(motorID, dxlIO.get_moving_speed([motorID])[0])
     elif (data[0] == 'get_present_load'):
-        print dxlIO.get_present_load([motorID])[0]
+        sendResponse(motorID, dxlIO.get_present_load([motorID])[0])
     elif (data[0] == 'get_present_position'):
-        print dxlIO.get_present_position([motorID])[0]
+        sendResponse(motorID, dxlIO.get_present_position([motorID])[0])
     elif (data[0] == 'get_present_position_speed_load'):
-        print dxlIO.get_present_position_speed_load([motorID])[0]
+        sendResponse(motorID, dxlIO.get_present_position_speed_load([motorID])[0])
     elif (data[0] == 'get_present_speed'):
-        print dxlIO.get_present_speed([motorID])[0]
+        sendResponse(motorID, dxlIO.get_present_speed([motorID])[0])
     elif (data[0] == 'get_present_temperature'):
-        print dxlIO.get_present_temperature([motorID])[0]
+        sendResponse(motorID, dxlIO.get_present_temperature([motorID])[0])
     elif (data[0] == 'get_present_voltage'):
-        print dxlIO.get_present_voltage([motorID])[0]
+        sendResponse(motorID, dxlIO.get_present_voltage([motorID])[0])
     elif (data[0] == 'get_return_delay_time'):
-        print dxlIO.get_return_delay_time([motorID])[0]
+        sendResponse(motorID, dxlIO.get_return_delay_time([motorID])[0])
     elif (data[0] == 'get_torque_limit'):
-        print dxlIO.get_torque_limit([motorID])[0]
+        sendResponse(motorID, dxlIO.get_torque_limit([motorID])[0])
     elif (data[0] == 'get_voltage_limit'):
-        print dxlIO.get_voltage_limit([motorID])[0]
+        sendResponse(motorID, dxlIO.get_voltage_limit([motorID])[0])
     elif (data[0] == 'is_led_on'):
-        print dxlIO.is_led_on([motorID])[0]
+        sendResponse(motorID, str(dxlIO.is_led_on([motorID])[0]))
     elif (data[0] == 'is_moving'):
-        print dxlIO.is_moving([motorID])[0]
+        sendResponse(motorID, str(dxlIO.is_moving([motorID])[0]))
     elif (data[0] == 'is_torque_enabled'):
-        print dxlIO.is_torque_enabled([motorID])[0]
+        sendResponse(motorID, str(dxlIO.is_torque_enabled([motorID])[0]))
     elif (data[0] == 'set_wheel_mode'):
         dxlIO.set_wheel_mode([motorID])
     elif (data[0] == 'set_joint_mode'):
@@ -118,17 +113,10 @@ def motorHandler(addr, tags, data, source):
     elif (data[0] == 'set_voltage_limit'):
         dxlIO.set_voltage_limit({motorID: [float(i) for i in data[1:]]}) # Range: [5.0 ; 25.0]V
 
-# This method of reporting timeouts only works by convention that before calling handle_request() field .timed_out is set to False
-def handleTimeout(self):
-    self.timed_out = True
-
-# User script that's called by the game engine every frame
-def processFrame():
-    global server
-    # Clear timed_out flag
-    server.timed_out = False
-    # Handle all pending requests then return
-    while not server.timed_out:
-        server.handle_request()
+def sendResponse(motorID, response):
+    global client
+    m = OSCMessage('/motor/' + str(motorID))
+    m.append(response)
+    client.send(m)
 
 if __name__ == '__main__': app()
---
+++ readme.md
@@ -0,0 +1,12 @@
+# DOS
+
+DOS (*Dynamixel OSC Server*) is a simple OSC server written in Python, providing an easy access to the core Dynamixel instructions.
+
+## API
+
+DOS is based on the PyPot API. The source code contains useful info about value ranges (for the Dynamixel AX-18 model).
+
+## Dependencies
+
+- PyPot (https://github.com/poppy-project/pypot)
+- pyOSC (https://github.com/ptone/pyosc)
